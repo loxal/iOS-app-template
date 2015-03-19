8798b254d5d51d243b5867e644a0378a9554fbfb
Alex <alexander.orlov@loxal.net>
2015-03-19 22:50:13 +0100

    update Kotlin version

--- src/main/kotlin/net/loxal/user/ios/view/RootViewController.kt
+++ src/main/kotlin/net/loxal/user/ios/view/RootViewController.kt
@@ -14,108 +14,31 @@
  * limitations under the License.
  */
 
-package net.loxal.example.kotlin.ios.view
+package net.loxal.user.ios.view
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.apache.http.HttpStatus
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.DefaultHttpClient
 import org.robovm.apple.coregraphics.CGRect
-import org.robovm.apple.uikit.UIButton
-import org.robovm.apple.uikit.UIButtonType
-import org.robovm.apple.uikit.UIColor
-import org.robovm.apple.uikit.UIControlState
+import org.robovm.apple.spritekit.SKView
+import org.robovm.apple.uikit.UIImage
+import org.robovm.apple.uikit.UIImageView
+import org.robovm.apple.uikit.UIScreen
 import org.robovm.apple.uikit.UIViewController
-
-import java.io.ByteArrayOutputStream
-import java.net.URI
-import net.loxal.user.ios.model.Host
-import org.robovm.apple.uikit.NSTextAlignment
-import org.robovm.apple.uikit.UIControlContentHorizontalAlignment
-import java.util.Date
-import org.robovm.apple.iad.ADBannerView
-import org.robovm.apple.iad.ADAdType
 import org.robovm.objc.annotation.CustomClass
-import org.robovm.apple.uikit.UILabel
-import org.robovm.apple.uikit.UIFont
 
 CustomClass("RootViewController")
-class RootViewController : UIViewController() {
+public class RootViewController : UIViewController() {
     private val mainView = getView()
-    private val infoContainer = UILabel()
-    private val timestamp = UILabel()
-    private val refresher = UIButton.create(UIButtonType.RoundedRect)
-    private val adBanner = ADBannerView(ADAdType.Banner)
+    private val skView: SKView
+    private val logo: UIImageView
 
-    private val mapper = ObjectMapper()
+    init {
+        skView = SKView()
+        skView.setFrame(UIScreen.getMainScreen().getApplicationFrame())
+        mainView.addSubview(skView)
 
-    private val httpClient = DefaultHttpClient()
-    private val uri: URI = URI.create("http://rest-kit-test-v1.test.cf.hybris.com/who-am-i")
-    private val httpGet: HttpGet = HttpGet(uri);
+        logo = UIImageView(CGRect(5.0, 10.0, 375.0, 220.0))
+        logo.setImage(UIImage.create("logo.png"))
+        skView.addSubview(logo)
 
-    {
-        mainView.setBackgroundColor(UIColor.white())
-
-        initRefreshUi()
-        initInfoContainer()
-        initInfoTimestamp()
-        initAdBanner()
-        refreshStatus()
-    }
-
-    private fun initInfoTimestamp() {
-        mainView.addSubview(timestamp)
-
-        timestamp.setFrame(CGRect(0.0, 120.0, mainView.getFrame().getMaxX(), 100.0))
-        timestamp.setTextAlignment(NSTextAlignment.Center)
-        timestamp.setFont(UIFont.getSystemFont(UIFont.getSystemFontSize()))
-    }
-
-    private fun initInfoContainer() {
-        mainView.addSubview(infoContainer)
-
-        infoContainer.setFrame(CGRect(0.0, 100.0, mainView.getFrame().getMaxX(), 20.0))
-        infoContainer.setTextAlignment(NSTextAlignment.Center)
-        infoContainer.setFont(UIFont.getSystemFont(UIFont.getSystemFontSize()))
-    }
-
-    private fun initRefreshUi() {
-        mainView.addSubview(refresher)
-
-        refresher.setFrame(CGRect(0.0, mainView.getFrame().getMidY(), mainView.getFrame().getMaxX(), 30.0))
-        refresher.setTitle("Refresh Status", UIControlState.Normal)
-        refresher.setContentHorizontalAlignment(UIControlContentHorizontalAlignment.Center)
-
-        refresher.addOnTouchUpInsideListener({ control, event -> refreshStatus() })
-    }
-
-    private fun refreshStatus() = showInfo(fetchHostInfo())
-
-    private fun showInfo(info: String) {
-        val host = mapper.readValue<Host>(info, javaClass<Host>())
-        infoContainer.setText("Host name: ${host.name}  IP address: ${host.address}")
-        timestamp.setText("Last refresh: ${Date().toGMTString()}")
-    }
-
-    private fun fetchHostInfo(): String {
-        ByteArrayOutputStream().use { out ->
-            val response = httpClient.execute(httpGet)
-            val status = response.getStatusLine()
-            val entity = response.getEntity()
-            if (HttpStatus.SC_OK == status.getStatusCode()) {
-                entity.writeTo(out)
-                return out.toString()
-            } else {
-                entity.getContent().close()
-            }
-        }
-
-        return "I’m very sorry, this should not happen."
-    }
-
-    private fun initAdBanner() {
-        mainView.addSubview(adBanner)
-
-        adBanner.setFrame(CGRect(0.0, mainView.getFrame().getMaxY() - adBanner.getFrame().getHeight(), 0.0, 0.0))
+        //        skView.text
     }
 }
