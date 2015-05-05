7467cfcd325679390491557640218020057b11ce
Alex <alexander.orlov@loxal.net>
2015-05-06 01:29:38 +0200

    hello world in SpriteKit

--- src/main/kotlin/net/loxal/user/ios/view/RootViewController.kt
+++ src/main/kotlin/net/loxal/user/ios/view/RootViewController.kt
@@ -16,10 +16,10 @@
 
 package net.loxal.user.ios.view
 
-import org.robovm.apple.coregraphics.CGRect
-import org.robovm.apple.spritekit.SKView
-import org.robovm.apple.uikit.UIImage
-import org.robovm.apple.uikit.UIImageView
+import org.robovm.apple.coregraphics.CGPoint
+import org.robovm.apple.coregraphics.CGSize
+import org.robovm.apple.spritekit.*
+import org.robovm.apple.uikit.UIColor
 import org.robovm.apple.uikit.UIScreen
 import org.robovm.apple.uikit.UIViewController
 import org.robovm.objc.annotation.CustomClass
@@ -27,18 +27,35 @@ import org.robovm.objc.annotation.CustomClass
 CustomClass("RootViewController")
 public class RootViewController : UIViewController() {
     private val mainView = getView()
-    private val skView: SKView
-    private val logo: UIImageView
+    private val skView: SKView = SKView()
 
     init {
-        skView = SKView()
+        val label = SKLabelNode()
+        label.setText("Hello, World!")
+        label.setFontSize(65.0)
+        label.setFontColor(UIColor.white())
+        label.setPosition(CGPoint(400.0, 784.0))
+
+        val rectangle = SKShapeNode()
+        rectangle.setPosition(CGPoint(400.0, 784.0))
+        rectangle.setGlowWidth(0.5)
+        rectangle.setLineWidth(0.5)
+        rectangle.setName("OHA")
+        rectangle.setFillColor(UIColor.blue())
+        //        rectangle.setStrokeColor(UIColor.green())
+
+        val scene = SKScene()
+        scene.addChild(rectangle)
+        scene.addChild(label)
+        scene.setBackgroundColor(UIColor.darkGray())
+        scene.setScaleMode(SKSceneScaleMode.AspectFill)
+        scene.setSize(CGSize(786.0, 1024.0))
+
+
         skView.setFrame(UIScreen.getMainScreen().getApplicationFrame())
+        skView.setShowsFPS(true)
+        skView.presentScene(scene)
+
         mainView.addSubview(skView)
-
-        logo = UIImageView(CGRect(5.0, 10.0, 375.0, 220.0))
-        logo.setImage(UIImage.create("logo.png"))
-        skView.addSubview(logo)
-
-        //        skView.text
     }
 }
